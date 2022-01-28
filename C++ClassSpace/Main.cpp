6b0e94b5bc19e98e718a97b0300f12b6fc32def1
R <642250219@qq.com>
2022-01-28 15:59:34 +0800

    年前更新

--- C++ClassSpace/Main.cpp
+++ C++ClassSpace/Main.cpp
@@ -57,13 +57,11 @@ int getSize()
 	int a = 6;
 	return  a;
 }
+StuInfo *stuInfo1;
 int main()
 {
-
 	StuInfo *stu1 = NULL;
 	
-	//cout << stu1.name << "..." << stu1.sex << "..." << stu1.score << endl;
-
 	StuInfo infos[6];
 
 	infos[0]=  { "Wang","男",99 };
@@ -72,41 +70,9 @@ int main()
 	infos[3] = { "He","男",569 };
 	infos[4] = { "Su","女",123 };
 	infos[5] = { "Su","女",123333 };
-	//memcpy(stu1, infos, sizeof(infos));
-	stu1 = infos;
-	//cout << infos[0].name<< endl;
-	int b = getSize();
-	//StuInfo c[b];
-	cout << sizeof(stu1) << endl;
-
-	//cout << &infos << endl;
-
-	//StuInfo *infoList = stu1;
-	//cout << infoList <<stu1<<"..."<< endl;  //指针infoList 与 &infos]拥有相同的地址
-	//cout <<  &infos << endl;
-	//cout << (sizeof(infos))/sizeof(infos[0])*sizeof(*infoList) << endl; //取指针代表的每个元素的长度*infoList
-	//for (int i = 0; i < 6; i++)
-	//{
-	//	cout << (infoList+i)->score << endl;
-	//	infoooo[i] = *(infoList + i);
-	//}
-
 
 	int a = 6;
-	//int *b = new int[a]; //使用指针数组
-	//vector<int> b(a);     //使用Vector数组
-	//for (size_t i = 0; i < a; i++)
-	//{
-	//	b[i] = i;
-	//	cout << b[i] << endl;
-	//}
-	//extern int c[];  ///声明一个数组，外部寻找
 
-	//delete b;
-	b = 0;
-
-	delete stu1;
-	stu1 = 0;
 	return 0;
 }
 
@@ -122,6 +88,8 @@ int main()
 //指针可为空，引用一定不为空
 
 //参数传递的3种方式：
+
+
 
 
 
